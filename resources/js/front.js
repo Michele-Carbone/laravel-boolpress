121e40424c876e7336a83b21bc7dab545099f11d
Michele-Carbone <michele.carboneetlab@gmail.com>
2021-10-21 18:24:01 +0800

    creazione e collegamento tra i vari file js e vue

---
+++ resources/js/front.js
@@ -0,0 +1,18 @@
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
+require('./bootstrap'); //portati bootstrap
+
+window.Vue = require('vue');
+
+
+//creaiamo il collegamento tra App.vue e il file front.js
+import App from "./components/App.vue";
+
+const app = new Vue({
+    el: '#root',
+    render: h => h(App)
+})
