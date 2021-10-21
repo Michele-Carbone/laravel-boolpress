454b402bd1c99e2f6c4f22cab2868ef73d0ceee7
Michele-Carbone <michele.carboneetlab@gmail.com>
2021-10-21 12:41:39 +0800

    first commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');  //i middleware sono delle funzione che accetta in ingresso la richiesta al termine della nostra richiesta decidono di restiuirci vero o falso (quindi se proseguire o no) se ce ne sono piu di una tutte seguiranno la stessa procedura
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('admin.home');  //pke abbiamo spostato la pagina home in admin
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('guest.welcome');   //pke abbiamo spostato la pagina welcome in guest
+});
+
+Auth::routes(['register' => false]);  //register =>false lo facciamo pke il blog che stiamo creando non dara' accesso ad altri users
+
+Route::get('/home', 'HomeController@index')->name('home');  //bisogna andare nella HomeController@index per modificare la route aggiungendo la cartella admin
