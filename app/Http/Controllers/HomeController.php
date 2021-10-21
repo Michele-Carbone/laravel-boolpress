05b90b8246b798c2381f8cc77a85621b5aaf1253
Michele-Carbone <michele.carboneetlab@gmail.com>
2021-10-21 13:23:44 +0800

    Rinomino la rotta e aggiungo middleware alla rotta stessa

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,15 +6,19 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
+    /* Essendo pubblico al momento possiamo anche togliere questa parte
     /**
      * Create a new controller instance.
      *
      * @return void
-     */
+    
+    
     public function __construct()
     {
         $this->middleware('auth');  //i middleware sono delle funzione che accetta in ingresso la richiesta al termine della nostra richiesta decidono di restiuirci vero o falso (quindi se proseguire o no) se ce ne sono piu di una tutte seguiranno la stessa procedura
     }
+    */
+
 
     /**
      * Show the application dashboard.
