6120b9ba9686a26f856b12c9490a0accd3d5aca6
Michele-Carbone <michele.carboneetlab@gmail.com>
2021-10-21 16:35:23 +0800

    creazione della tabella

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -3,6 +3,30 @@
 
 @section('content')
     
+    <div class="container">
+        <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">Title</th>
+                <th scope="col">Scritto il</th>
+                <th scope="col"></th>
+              </tr>
+            </thead>
+            <tbody>
+                @forelse ($posts as $post)
+                <tr>
+                    <td>{{ $post->title }}</td>
+                    <td>{{ $post->created_at }}</td>
+                    <td><a href="{{ route('admin.posts.show', $post->id) }}" class="btn btn-primary">Vai</a></td>
+                </tr>
+                @empty
+                <tr>
+                    <td colspan="3" class="text-center">Non ci sono post da visualizzare</td>
+                </tr>
+                @endforelse
+              
+          </table>
+    </div>
 @endsection
 
 
