5a135784b5e065a9843e37b12905f15c439add8c
Michele-Carbone <michele.carboneetlab@gmail.com>
2021-10-22 22:10:50 +0800

    correzione errore su confirm-delete.js

--- public/js/confirm-delete.js
+++ public/js/confirm-delete.js
@@ -1,9 +1,13 @@
 
+
+
 const deleteButtons = document.querySelectorAll('.delete-button');  //querySelectorAll() ci permette di prendere piu di un elemento
 
 deleteButtons.forEach(form => {
     form.addEventListener('submit', function (event) {
-        event.preventDefult();
+        console.dir('Premuto');
+        event.preventDefault();
+
         const conf = confirm('Sei sicuro di voler cancellare questo post?');
         if (conf) this.submit();
     });
