0e7a5bbd335e38af0822bd5b5da19dc5177569e9
Michele-Carbone <michele.carboneetlab@gmail.com>
2021-10-26 10:30:56 +0800

    creazione colonna user_id in Db con migrate

---
+++ database/migrations/2021_10_26_022711_add_user_id_to_posts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddUserIdToPostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            //relazione tra users
+            $table->foreignId('user_id')->after('id')->nullable('set null')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            //
+            $table->dropForeign('posts_user_id_foreign');
+
+            //eliminare la colonna
+            $table->dropColumn('user_id');
+        });
+    }
+}
